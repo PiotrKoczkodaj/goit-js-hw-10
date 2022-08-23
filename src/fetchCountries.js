97d7023eee1fa472af39bb7abb0cf0df8384f342
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-08-23 10:11:28 +0200

    .

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-const  fetchCountries(name) => {
+const  fetchCountries = (name) => {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
     .then(response => {
         if (!response.ok) {
